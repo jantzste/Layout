577be9691b4100fafac78db2e5742dfb3c3d2f5d
Stephen Jantz <sjantz@hotmail.de>
2017-08-26 13:23:53 +0200

    first init

---
+++ Taschenrechner/MenuViewController.swift
@@ -0,0 +1,84 @@
+//
+//  MenuViewController.swift
+//  Taschenrechner
+//
+//  Created by Stephen Japon Jantz on 15.07.17.
+//  Copyright © 2017 Stephen Jantz. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNaviBar()
+        
+        //add scrollView
+        let renderScroll = renderScrollView()
+        let scrollView = renderScroll.scrollView()
+        self.view.addSubview(scrollView)
+        
+        setTitel()
+        
+        
+    }
+    
+    //add a title above of the scrollView
+    func setTitel(){
+        let titel = UILabel(frame: CGRect(x: 10, y: 85, width: 200, height: 21))
+        titel.textAlignment = .left
+        titel.text = "Verlauf"
+        titel.textColor = UIColor.black
+        titel.font = titel.font.withSize(20)
+        
+        self.view.addSubview(titel)
+        
+        
+    }
+    
+
+    //add the navigationBar
+    func setNaviBar(){
+        view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+        //make the screenSize automatically
+        let screenSize: CGRect = UIScreen.main.bounds
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: screenSize.width , height: 60))
+        
+        let navItem = UINavigationItem(title: "Rechner")
+ 
+        let backButton = UIBarButtonItem(title: "Zurück", style: UIBarButtonItemStyle.plain, target: self, action: #selector (cancelButton))
+        
+        navItem.leftBarButtonItem = backButton
+        navBar.setItems([navItem], animated: false)
+        self.view.addSubview(navBar)
+        
+        
+        //change the color color of Navigationbar to white
+        UINavigationBar.appearance().tintColor = UIColor.white
+        navBar.barTintColor = UIColor.black
+        navBar.isTranslucent = false
+    }
+    
+    
+    //Backbutton
+    func cancelButton(){
+        
+        dismiss(animated: false, completion: nil)
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
+extension UIColor{
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
+
---
+++ Taschenrechner/renderScrollView.swift
@@ -0,0 +1,34 @@
+//
+//  renderScrollView.swift
+//  Taschenrechner
+//
+//  Created by Stephen Japon Jantz on 28.07.17.
+//  Copyright © 2017 Stephen Jantz. All rights reserved.
+//
+
+import UIKit
+
+class renderScrollView: UIViewController {
+    
+
+    //create a ScrollView with that properties
+    func scrollView() -> UIScrollView{
+        let grayScrollView = UIScrollView(frame: CGRect(x: 0, y: 130, width: 420, height: 610))
+        
+        //let blueScrollView = UIScrollView(frame: CGRect(x: 10, y: 200, width: 400, height: 400))
+        grayScrollView.backgroundColor = UIColor(r: 239, g: 240, b: 241)
+        grayScrollView.contentOffset = CGPoint(x: 10, y: 20)
+        grayScrollView.contentSize = CGSize(width: 100, height: 100)
+
+        
+        //add a white CardViw
+        let whiteCardView = UIView(frame: CGRect(x: 12, y: 20, width: 390, height: 580))
+        whiteCardView.backgroundColor = UIColor.white
+        whiteCardView.layer.cornerRadius = 5
+        grayScrollView.addSubview(whiteCardView)
+        
+        return grayScrollView
+        
+    }
+
+}
